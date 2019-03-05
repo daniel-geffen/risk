94563b68961b75db9b25f35c68cf4d1f5453a529
Daniel Geffen <geffen.daniel@gmail.com>
2019-03-05 13:03:13 +0200

    Add continents and neighbors

--- web/index.js
+++ web/index.js
@@ -16,14 +16,15 @@ const serverResponse = {
     }
 };
 
-let highlight;
-let doc;
+let svgDoc, json;
+$.getJSON('continents.json', obj => json = obj);
+
 
 initPlayerCountries = (playerData, playerName) => {
     $('#legendList').append($(`<li style="color:${playerData.color}" />`).text(playerName));
 
     _.forEach(playerData.countries, (numOfSoldiers, countryName) => {
-        const country = doc.getElementById(countryName);
+        const country = svgDoc.getElementById(countryName);
         country.setAttribute('fill', playerData.color);
         const boundingBox = country.getBBox();
         const textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text');
@@ -38,32 +39,55 @@ initCountry = country => {
     country.addEventListener('mouseover', mouseoverCountry);
 };
 
+removeNeighbors = () => {
+    const map = svgDoc.getElementById('map');
+    while (map.getElementsByClassName('neighbor').length) {
+        map.removeChild(map.getElementsByClassName('neighbor')[0]);
+    }
+};
+
 mouseoverCountry = evt => {
     const country = evt.target;
-    const outline = country.getAttribute( 'd' );
-    highlight.setAttribute( 'd', outline );
-    $('#countryName').text(country.getAttribute( 'id' ));
+    const outline = country.getAttribute('d');
+
+    const countryHighlight = svgDoc.getElementById('Country Highlight');
+    const countryName = country.getAttribute('id');
+    const continentName = _.findKey(json, countryObj => _.includes(_.keys(countryObj), countryName));
+
+    countryHighlight.setAttribute( 'd', outline);
+    $('#countryName').text(countryName);
+    $('#continentName').text(continentName);
+    removeNeighbors();
+
+    _.forEach(json[continentName][countryName]['neighbors'], neighborId => {
+        const neighborName = _.findKey(_.merge(..._.values(json)), {id: neighborId});
+        const neighborOutline = svgDoc.getElementById(neighborName).getAttribute('d');
+        const neighborHighlight = countryHighlight.cloneNode(true);
+        neighborHighlight.setAttribute('d', neighborOutline);
+        neighborHighlight.setAttribute('id', neighborId);
+        neighborHighlight.setAttribute('class', 'neighbor');
+        neighborHighlight.setAttribute('opacity', '0.2');
+        neighborHighlight.style['pointer-events'] = 'none';
+        svgDoc.getElementById('map').appendChild(neighborHighlight);
+    })
 };
 
 mouseoverSea = evt => {
     const sea = evt.target;
     $('#countryName').text(sea.getAttribute( 'id' ));
-    highlight.setAttribute( 'd', 'm0 0' );
+    $('#continentName').text('');
+    svgDoc.getElementById('Country Highlight').setAttribute( 'd', 'm0 0' );
+    removeNeighbors();
 };
 
 init = () => {
     const svg = document.getElementById('mapObject').contentDocument.getElementById('mapSvg');
-    doc = svg.ownerDocument;
-    highlight = doc.getElementById('highlight');
+    svgDoc = svg.ownerDocument;
 
     _.forEach(serverResponse, initPlayerCountries);
 
-    const countries = doc.getElementsByClassName('country');
-
-    _.forEach(countries, initCountry);
-    const seas = doc.getElementsByClassName('sea');
-
-    _.forEach(seas, sea => sea.addEventListener('mouseover', mouseoverSea));
+    _.forEach(svgDoc.getElementsByClassName('country'), initCountry);
+    _.forEach(svgDoc.getElementsByClassName('sea'), sea => sea.addEventListener('mouseover', mouseoverSea));
 };
 
 window.addEventListener('load', init);
