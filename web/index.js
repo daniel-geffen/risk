272570526d9eb769c6974f0c633cfde36989e6bc
Daniel Geffen <geffen.daniel@gmail.com>
2019-03-17 16:46:23 +0200

    Conquer countries

--- web/index.js
+++ web/index.js
@@ -14,11 +14,15 @@ handleUsername = usernameInput => {
     webSocket.send(JSON.stringify(message));
 };
 
-mouseenterCountry = evt => {
+getRivalNeighbors = countryName => {
+    const currentPlayerCountryIds = _.map(_.pick(countriesJson, _.keys(gameState.players[gameState.currentPlayer].countries)), 'id');
+    return _.difference(countriesJson[countryName].neighbors, currentPlayerCountryIds);
+};
+
+mouseEnterCountry = evt => {
     const country = $(evt.target);
     const countryName = country.attr('id');
 
-    country.css('cursor', 'pointer');
     $('#highlight', svgDoc).attr('d', country.attr('d'));
     const continentName = countriesJson[countryName].continent;
 
@@ -26,8 +30,7 @@ mouseenterCountry = evt => {
     $('#continentName').text(continentName);
     $('#map', svgDoc).children('.neighbor').remove();
 
-    const currentPlayerCountries = _.keys(gameState.players[gameState.currentPlayer].countries);
-    const rivalNeighbors = _.difference(countriesJson[countryName].neighbors, _.map(_.pick(countriesJson, currentPlayerCountries), 'id'));
+    const rivalNeighbors = getRivalNeighbors(countryName);
     _.forEach(rivalNeighbors, neighborId => {
         const neighborName = _.findKey(countriesJson, {id: neighborId});
         const neighborOutline = $(`[id='${neighborName}']`, svgDoc).attr('d');
@@ -47,11 +50,49 @@ mouseLeaveCountry = () => {
     $('#map', svgDoc).children('.neighbor').remove();
 };
 
-makeCurrentPlayerCountriesResponsive = currentPlayerCountries => {
-    $('.country').unbind();
+mouseEnterLeaveNeighbor = evt => {
+    const neighborName = $(evt.target).attr('id');
+    $(`#${countriesJson[neighborName].id}`, svgDoc).toggleClass('selectedNeighbor');
+};
+
+conquerCountry = (defendingCountry, troopsMoved) => {
+    const defendingCountryId = countriesJson[defendingCountry.attr('id')].id;
+    $(`#${defendingCountryId}`, svgDoc).remove();
+    $(`#${defendingCountryId}Text`, svgDoc).text(troopsMoved);
+    defendingCountry.attr('fill', gameState.players[gameState.currentPlayer].color);
+};
+
+mouseClickNeighbor = (attackingCountry, evt) => {
+    const defendingCountry = $(evt.target);
+    const attackingCountryId = countriesJson[attackingCountry].id;
+    const defendingCountryId = countriesJson[defendingCountry.attr('id')].id;
+
+    const troopsToMove = prompt("Enter number of troops to move to conquered country");
+    const attackingText = $(`#${attackingCountryId}Text`, svgDoc);
+    attackingText.text(attackingText.text() - troopsToMove);
+    conquerCountry(defendingCountry, troopsToMove);
+};
+
+makeCurrentPlayerCountriesResponsive = () => {
+    $('.country', svgDoc).unbind();
+
+    const currentPlayerCountries = _.keys(gameState.players[gameState.currentPlayer].countries);
     const responsiveCountries = $(_.join(_.map(currentPlayerCountries, c => `[id='${c}']`), ', '), svgDoc);
-    responsiveCountries.mouseenter(mouseenterCountry);
+    responsiveCountries.mouseenter(mouseEnterCountry);
+    responsiveCountries.click(mouseClickCountry);
     responsiveCountries.mouseleave(mouseLeaveCountry);
+};
+
+mouseClickCountry = evt => {
+    $('.country', svgDoc).unbind();
+
+    const selectedCountry = $(evt.target);
+    selectedCountry.click(makeCurrentPlayerCountriesResponsive);
+    const attackingTargets = _.map(getRivalNeighbors(selectedCountry.attr('id')), id => _.findKey(countriesJson, {id}));
+    const attackingTargetsSelector = _.join(_.map(attackingTargets, c => `[id='${c}']`), ', ');
+    $(attackingTargetsSelector, svgDoc).mouseenter(mouseEnterLeaveNeighbor);
+    $(attackingTargetsSelector, svgDoc).mouseleave(mouseEnterLeaveNeighbor);
+    $(attackingTargetsSelector, svgDoc).click(_.partial(mouseClickNeighbor, selectedCountry.attr('id')));
 };
 
 addPlayerListItem = (playerData, playerName, message) => {
@@ -68,6 +109,7 @@ paintCountry = (color, numOfTroops, countryName) => {
     country.attr('fill', color);
     const boundingBox = country[0].getBBox();
     const textElement = $(document.createElementNS('http://www.w3.org/2000/svg', 'text')).attr({
+        id: `${countriesJson[countryName].id}Text`,
         transform: `translate(${boundingBox.x + boundingBox.width / 2} ${boundingBox.y + boundingBox.height / 2})`,
         class: 'countryText'
     }).text(numOfTroops);
@@ -79,7 +121,7 @@ onServerMessage = evt => {
 
     $('#title').text(`${gameState.currentPlayer}'s turn`);
     if (gameState.currentPlayer === username)
-        makeCurrentPlayerCountriesResponsive(_.keys(gameState.players[gameState.currentPlayer].countries));
+        makeCurrentPlayerCountriesResponsive();
     _.forEach(gameState.players, (playerData, playerName) => {
         addPlayerListItem(playerData, playerName, gameState);
 
